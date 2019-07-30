fc5f7a766f8a081b1ab751792a32ed65a84427ed
helenkatek <oli.tal2001@gmail.com>
2019-07-30 16:38:28 +0900

    UART echo project takes value from PC to the board through UART Rx and present the value through UART TX module.

--- uart/uart.py
+++ uart/uart.py
@@ -11,14 +11,20 @@ ser.open()
 
 while True:
 	try: 
-		data_to_send = input('Enter a number x between 0 and 7 => ')
+		data_to_send = input('Enter a number x between 0 and 255 => ')
 		data_to_send = int(data_to_send)
-		if (0 <= data_to_send) and (data_to_send < 8) :  
+		if (0 <= data_to_send < 256) :  # editted for echo
 			data_byte = (data_to_send).to_bytes(1, byteorder="little")
 			ser.write(data_byte)
 			time.sleep(0.1)
+
+			####### added for echo  #######
+			data_read = ser.read(1)
+			data_read = int.from_bytes(data_read, byteorder="little")
+			print (data_read)
+			###############################
 		else :
-			raise ValueError('x should not be less than 8!')
+			raise ValueError('x should not be less than 256!')
 			
 	except KeyboardInterrupt:
 		break
---
+++ uart/uart_led_echo.sv
@@ -0,0 +1,59 @@
+`timescale 1ns / 1ps
+module uart_led_echo
+	(input logic  usrclk_n, usrclk_p,
+	 input logic rx_data_in,
+	 output logic tx_data_out);
+
+// uart rx signals
+logic clk;
+logic [7:0] data_reg;
+logic rx_ready;							// rx ready signal
+logic rx_temp, rx_sync;					// temprary and synchronized rxs
+logic [7:0] rx_data_out;				// output data from uart
+// uart tx signals
+logic [7:0] tx_data_in;
+logic tx_ready;
+logic echoed_value;
+
+logic tx_active, tx_done;
+// clocking module
+clk_wiz_0 clk_module(
+	.clk_in1_n(usrclk_n),
+	.clk_in1_p(usrclk_p),
+	.clk_out1(clk));
+// uart rx module
+uart_rx #(.CLKS_PER_BIT(87)) uart_rx ( 
+	.i_Clock(clk),
+	.i_Rx_Serial(rx_sync),
+	.o_Rx_DV(rx_ready),
+	.o_Rx_Byte(rx_data_out));
+// uart tx_module
+uart_tx #(.CLKS_PER_BIT(87)) uart_tx (
+	.i_Clock(clk),
+	.i_Tx_DV(tx_ready),
+	.i_Tx_Byte(tx_data_in),
+	.o_Tx_Active(tx_active),
+	.o_Tx_Serial(tx_data_out),
+	.o_Tx_Done(tx_done));
+
+// syncronizer
+always @(posedge clk)
+	begin
+		rx_temp <= rx_data_in; 				
+		rx_sync <= rx_temp;
+	end
+
+always @(posedge clk)
+	if (rx_ready) data_reg <= rx_data_out;
+
+always @(posedge clk)
+	tx_ready <= rx_ready;
+
+assign tx_data_in = data_reg;	
+	
+always @(posedge clk)
+	if (tx_done) echoed_value = tx_data_out;
+
+
+
+endmodule
