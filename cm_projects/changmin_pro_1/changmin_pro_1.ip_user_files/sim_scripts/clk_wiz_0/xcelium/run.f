d150f04269ff36958b279716142c2ca16a738454
helenkatek <oli.tal2001@gmail.com>
2019-09-02 17:04:56 +0900

    Changmin's projects

---
+++ cm_projects/changmin_pro_1/changmin_pro_1.ip_user_files/sim_scripts/clk_wiz_0/xcelium/run.f
@@ -0,0 +1,8 @@
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../changmin_pro_1.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0_clk_wiz.v" \
+  "../../../../changmin_pro_1.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0.v" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  glbl.v
+-endlib
+
